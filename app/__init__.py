78452de291edb9610baba7807acb621464004109
SatoshiOhashi1998 <nagaomasaki.lauren@gmail.com>
2026-09-10 15:50:00 +0900

    feat: インスタンスパスを使用する形に変更した。

--- app/__init__.py
+++ app/__init__.py
@@ -1,33 +1,11 @@
 """
 Flaskアプリケーションモジュール
-
-このモジュールは、Flaskを使用してWebアプリケーションを作成します。アプリケーションは、指定されたテンプレートフォルダと静的ファイルフォルダを使用し、CORSを有効化します。
-また、URLスケジューラーとログ機能が統合されています。
-
-使用方法:
-1. Flaskアプリケーションを作成するために、`create_app()` 関数を呼び出します。
-2. 必要に応じて、アプリケーションの設定を変更します。
-3. アプリケーションを実行するには、`flask run` を使用します。
-
-依存関係:
-- Flask: Webアプリケーションを構築するためのフレームワーク。
-- flask_cors: CORS (Cross-Origin Resource Sharing)を有効にするためのライブラリ。
-- app.log: ロギングの設定を行うためのカスタムモジュール。
-- app.views.web: 画面描画用ルーティングを定義するためのカスタムモジュール。
-- app.views.api: API用ルーティングを定義するためのカスタムモジュール。
-- app.scheduler: URLスケジューリングを管理するためのカスタムモジュール。
-
-設定内容:
-- セッションの持続時間は5分に設定されています。
-- テンプレートフォルダは 'templates' に、静的ファイルフォルダは 'static' に指定されています。
-
 """
 
-import os
-from pathlib import Path
 from datetime import timedelta
 from flask import Flask
 from flask_cors import CORS
+
 from app.log import setup_logging
 from app.views.web import web
 from app.views.api import api_bp
@@ -57,7 +35,7 @@ def create_app(test_config=None):
 
     # DB設定
     if 'SQLALCHEMY_DATABASE_URI' not in app.config:
-        DB_PATH = Path(__file__).resolve().parent / 'video_data.db'
+        DB_PATH = app.instance_path / 'video_data.db'
         app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_PATH}"
 
     app.config.setdefault(
